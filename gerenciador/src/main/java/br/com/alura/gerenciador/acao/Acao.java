d92c4468e4240330eafed5d90176a4ca98db9be1
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-09-27 00:57:42 -0300

    atualizando projetos

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/Acao.java
@@ -0,0 +1,17 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public interface Acao {
+
+	String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+		
+}
